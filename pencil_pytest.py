817d65ddce4f0a9923af6776345742e90be58a4c
MeridithJunk <Meridith.a.junk@accenture.com>
2022-03-02 15:31:42 -0600

    failing test

--- pencil_pytest.py
+++ pencil_pytest.py
@@ -6,6 +6,10 @@ class TestPencil(unittest.TestCase):
     def test_when_ISupplyPencilWithAString_ThenTheStringIsReturned(self):
         self.assertEqual(pencil.write("She sells sea shells"), "She sells sea shells")
 
+    def test_when_ISupplyPencilWithTwoStrings_ThenTheStringAppendedAndIsReturned(self):
+        pencil.write("She sells sea shells")
+        self.assertEqual(pencil.write(" by the seashore"), "She sells sea shells by the seashore")
+
 
 if __name__ == '__main__':
     unittest.main()
